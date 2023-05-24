7910a865dd912bbf5b3d81908bc71b14f43345de
Dor Nudel <dornu23@gmail.com>
2023-05-24 11:06:13 +0300

    RoomOutline creation

--- Assets/Scripts/Rooms/LevelGenerator.cs
+++ Assets/Scripts/Rooms/LevelGenerator.cs
@@ -2,8 +2,210 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelGenerator : MonoBehaviour
 {
-    
+    public enum Direction
+    {
+        Up,
+        Right,
+        Down,
+        Left,
+    };
+
+    [SerializeField] private GameObject layoutRoom;
+    [SerializeField] private Color startColor;
+    [SerializeField] private Color endColor;
+    [SerializeField] private int distanceToEnd;
+    [SerializeField] private Transform generatorPoint;
+    [SerializeField] private Direction selectedDirection;
+    [SerializeField] private float xOffset = 18f;
+    [SerializeField] private float yOffset = 10f;
+    [SerializeField] private LayerMask roomLayoutLayer;
+    [SerializeField] private RoomPrefabs rooms;
+
+    private GameObject endRoom;
+    private List<GameObject> layoutRoomObjects = new List<GameObject>();
+    private List<GameObject> generatedOutlines = new List<GameObject>();
+
+    private void Awake()
+    {
+        GameObject startRoom = Instantiate(layoutRoom, generatorPoint.position, generatorPoint.rotation);
+        startRoom.GetComponent<SpriteRenderer>().color = startColor;
+
+        selectedDirection = (Direction)UnityEngine.Random.Range(0, 4);
+        MoveGenerationPoint();
+
+        for (int i = 0; i < distanceToEnd; i++)
+        {
+            GameObject newRoom = Instantiate(layoutRoom, generatorPoint.position, generatorPoint.rotation);
+            layoutRoomObjects.Add(newRoom);
+
+            if (i + 1 == distanceToEnd)
+            {
+                newRoom.GetComponent<SpriteRenderer>().color = endColor;
+                layoutRoomObjects.RemoveAt(layoutRoomObjects.Count - 1);
+                endRoom = newRoom;
+            }
+
+            selectedDirection = (Direction)UnityEngine.Random.Range(0, 4);
+            MoveGenerationPoint();
+
+            while (Physics2D.OverlapCircle(generatorPoint.position, 0.2f, roomLayoutLayer))
+            {
+                MoveGenerationPoint();
+            }
+        }
+
+        // Create room outline
+        CreateRoomOutline(Vector3.zero);
+        foreach (GameObject room in layoutRoomObjects)
+        {
+            CreateRoomOutline(room.transform.position);
+        }
+        CreateRoomOutline(endRoom.transform.position);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.R))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+
+    public void MoveGenerationPoint()
+    {
+        switch (selectedDirection)
+        {
+            case Direction.Up:
+                generatorPoint.position += new Vector3(0f, yOffset, 0f);
+                break;
+            case Direction.Down:
+                generatorPoint.position += new Vector3(0f, -yOffset, 0f);
+                break;
+            case Direction.Right:
+                generatorPoint.position += new Vector3(xOffset, 0f, 0f);
+                break;
+            case Direction.Left:
+                generatorPoint.position += new Vector3(-xOffset, 0f, 0f);
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void CreateRoomOutline(Vector3 roomPosition)
+    {
+        bool roomAbove = Physics2D.OverlapCircle(roomPosition + new Vector3(0f, yOffset, 0f), 0.2f, roomLayoutLayer);
+        bool roomBelow = Physics2D.OverlapCircle(roomPosition + new Vector3(0f, -yOffset, 0f), 0.2f, roomLayoutLayer);
+        bool roomLeft = Physics2D.OverlapCircle(roomPosition + new Vector3(-xOffset, 0f, 0f), 0.2f, roomLayoutLayer);
+        bool roomRight = Physics2D.OverlapCircle(roomPosition + new Vector3(xOffset, 0f, 0f), 0.2f, roomLayoutLayer);
+
+        int directionCount = 0;
+        if (roomAbove)
+        {
+            directionCount++;
+        }
+        if (roomBelow)
+        {
+            directionCount++;
+        }
+        if (roomLeft)
+        {
+            directionCount++;
+        }
+        if (roomRight)
+        {
+            directionCount++;
+        }
+
+        switch (directionCount)
+        {
+            case 0:
+                Debug.LogError("Found no rooms!!!");
+                break;
+            case 1:
+                if (roomAbove)
+                {
+                    generatedOutlines.Add(Instantiate(rooms.singleUp, roomPosition, transform.rotation));
+                }
+                if (roomBelow)
+                {
+                    generatedOutlines.Add(Instantiate(rooms.singleDown, roomPosition, transform.rotation));
+                }
+                if (roomLeft)
+                {
+                    generatedOutlines.Add(Instantiate(rooms.singleLeft, roomPosition, transform.rotation));
+                }
+                if (roomRight)
+                {
+                    generatedOutlines.Add(Instantiate(rooms.singleRight, roomPosition, transform.rotation));
+                }
+                break;
+            case 2:
+                if (roomAbove && roomBelow)
+                {
+                    generatedOutlines.Add(Instantiate(rooms.doubleUpDown, roomPosition, transform.rotation));
+                }
+                if (roomAbove && roomLeft)
+                {
+                    generatedOutlines.Add(Instantiate(rooms.doubleLeftUp, roomPosition, transform.rotation));
+                }
+                if (roomAbove && roomRight)
+                {
+                    generatedOutlines.Add(Instantiate(rooms.doubleUpRight, roomPosition, transform.rotation));
+                }
+                if (roomBelow && roomLeft)
+                {
+                    generatedOutlines.Add(Instantiate(rooms.doubleDownLeft, roomPosition, transform.rotation));
+                }
+                if (roomBelow && roomRight)
+                {
+                    generatedOutlines.Add(Instantiate(rooms.doubleRightDown, roomPosition, transform.rotation));
+                }
+                if (roomLeft && roomRight)
+                {
+                    generatedOutlines.Add(Instantiate(rooms.doubleLeftRight, roomPosition, transform.rotation));
+                }
+                break;
+            case 3:
+                if (roomAbove && roomBelow && roomRight)
+                {
+                    generatedOutlines.Add(Instantiate(rooms.tripleUpRightDown, roomPosition, transform.rotation));
+                }
+                if (roomLeft && roomBelow && roomRight)
+                {
+                    generatedOutlines.Add(Instantiate(rooms.tripleRightDownLeft, roomPosition, transform.rotation));
+                }
+                if (roomAbove && roomBelow && roomLeft)
+                {
+                    generatedOutlines.Add(Instantiate(rooms.tripleDownLeftUp, roomPosition, transform.rotation));
+                }
+                if (roomAbove && roomLeft && roomRight)
+                {
+                    generatedOutlines.Add(Instantiate(rooms.tripleLeftUpRight, roomPosition, transform.rotation));
+                }
+                break;
+            case 4:
+                if (roomAbove && roomBelow && roomLeft && roomRight)
+                {
+                    generatedOutlines.Add(Instantiate(rooms.fourway, roomPosition, transform.rotation));
+                }
+                break;
+
+            default:
+                break;
+        }
+    }
+}
+
+[System.Serializable]
+public class RoomPrefabs
+{
+    public GameObject singleUp, singleDown, singleRight, singleLeft,
+                      doubleUpDown, doubleLeftRight, doubleUpRight, doubleRightDown, doubleDownLeft, doubleLeftUp,
+                      tripleUpRightDown, tripleRightDownLeft, tripleDownLeftUp, tripleLeftUpRight,
+                      fourway;
 }
